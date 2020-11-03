2dd0a667c294401c99f96e79287fb58a3fa36716
wBinek <wbinek@agh.edu.pl>
2020-11-03 23:21:17 +0100

    Add notebook helpers to simplify notebook

---
+++ notebook_helpers/helpers.py
@@ -0,0 +1,59 @@
+import pythreejs as pts
+import numpy as np
+
+global camera
+camera = None
+
+def display_model(model_display_holder, model3D, simulation):
+    #Translate room model to 3d compatible format
+    vertices = [[vert.x, vert.y, vert.z] for vert in model3D.vertices] 
+    normals = [[norm.x, norm.y, norm.z] for norm in model3D.normals]
+    materials = model3D.materials
+    faces = [f.vertices + [normals[f.normal_idx], materials[f.mat_name]['kd_hex'], None] for f in model3D.faces]
+    viewGeometry = pts.Geometry(vertices=vertices, faces=faces)
+    meshModel = pts.Mesh(
+        geometry=viewGeometry,
+        material=pts.MeshLambertMaterial(vertexColors='VertexColors',
+                                             side='FrontSide'))
+
+    #Calculate model centroid
+    vert_min = np.min(np.array(vertices), axis=0)
+    vert_max = np.max(np.array(vertices), axis=0)
+    center = ((vert_min + vert_max)/2).tolist()
+    
+    # Translate sources to red spheres
+    if(simulation.source):
+        s_disp_pos = [simulation.source.position.x, simulation.source.position.y, simulation.source.position.z]
+        dsource = pts.Mesh(geometry=pts.SphereGeometry(),material=pts.MeshLambertMaterial(color='red'),position=s_disp_pos)
+    else:
+        dsource=None
+        
+    # Translate receivers to red spheres
+    if(simulation.receiver):
+        r_disp_pos = [simulation.receiver.position.x, simulation.receiver.position.y, simulation.receiver.position.z]
+        dreceiver = pts.Mesh(geometry=pts.SphereGeometry(),material=pts.MeshLambertMaterial(color='green'),position=r_disp_pos)
+    else:
+        dreceiver=None
+            
+    # Generate camera if not avalible
+    global camera    
+    if not camera:
+        key_light = pts.DirectionalLight(color='white', position=[30,2,-15], intensity=0.9)
+        camera = pts.PerspectiveCamera(position=[50, 10, -20], up=[0, 1, 0], children=[key_light], aspect=2)
+    
+    # Build scene from all objects
+    scene = pts.Scene(children=[meshModel, dsource, dreceiver, camera, pts.AmbientLight(color='#777777')], background=None)
+
+    # Generate renderer
+    renderer = pts.Renderer(camera=camera,
+                        scene=scene,
+                        alpha=True,
+                        clearOpacity=0,
+                        width=800,
+                        height = 400,
+                        controls=[pts.OrbitControls(controlling=camera, target=center)])
+    
+    #Acquire place for model display and generate image
+    model_display_holder.clear_output()
+    with model_display_holder:
+        display(renderer)
